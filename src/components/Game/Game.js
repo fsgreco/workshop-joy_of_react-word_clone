aece8e938e702872c533b06b295ed5387ed6b31f
Santiago <fsgreco@fastmail.com>
2023-06-17 19:22:35 +0200

    Final Game.js

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,14 +2,34 @@ import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants'
 
-// Pick a random word on every pageload.
-const answer = sample(WORDS);
-// To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
+import GuessForm from '../GuessForm'
+import GuessGrid from '../GuessGrid'
+import ResultBanner from '../../components/ResultBanner'
 
 function Game() {
-  return <>Put a game here!</>;
+	
+	// Pick a random word on every mount.
+	const [ answer, setAnswer ] = React.useState( sample(WORDS) )
+	const [ words, setWords ] = React.useState([])
+	console.info({ answer });
+	
+	const restartGameFn = () => {
+		setAnswer( sample(WORDS) )
+		setWords([])
+	}
+
+	let canStillPlay = words.length < NUM_OF_GUESSES_ALLOWED
+	let checkWin = words.includes(answer)
+
+  return <>
+		<GuessGrid words={words} answer={answer} />
+		<GuessForm disabled={!canStillPlay || checkWin } AddWordsCallback={setWords} />
+
+		{ checkWin && <ResultBanner wins guessesNumber={words.length} restartFn={restartGameFn} /> }
+		{!canStillPlay && <ResultBanner wins={checkWin} guessesNumber={words.length} answer={answer} restartFn={restartGameFn}/> }
+	</>
 }
 
 export default Game;
